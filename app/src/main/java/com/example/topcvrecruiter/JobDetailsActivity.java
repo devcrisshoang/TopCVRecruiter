8c7061a64ec92d91d3186dad201c282c5d8047cf
DevCriss <crisshoang0711@gmail.com>
2024-10-08 23:29:08 +0700

    create layout to post article and job

--- app/src/main/java/com/example/topcvrecruiter/JobDetailsActivity.java
+++ app/src/main/java/com/example/topcvrecruiter/JobDetailsActivity.java
@@ -1,7 +1,9 @@
 package com.example.topcvrecruiter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -10,19 +12,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ArticleActivity extends AppCompatActivity {
+public class JobDetailsActivity extends AppCompatActivity {
+    private Button post_button;
     private ImageButton back_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_article);
+        setContentView(R.layout.activity_job_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        post_button = findViewById(R.id.post_button);
         back_button = findViewById(R.id.back_button);
+        post_button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
         back_button.setOnClickListener(view -> {
             finish();
         });
