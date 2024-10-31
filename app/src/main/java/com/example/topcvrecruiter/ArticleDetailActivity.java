19613ab3e19b396990b4a96f2018a150f20cc4a0
CrissDev <crisshoang0711@gmail.com>
2024-11-01 00:02:30 +0700

    job and article detail user interface

---
+++ app/src/main/java/com/example/topcvrecruiter/ArticleDetailActivity.java
@@ -0,0 +1,43 @@
+package com.example.topcvrecruiter;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class ArticleDetailActivity extends AppCompatActivity {
+    private int articleId;
+    private TextView name;
+    private TextView content;
+    private Disposable disposable;
+    private ImageView image;
+    private ImageButton back_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_article_detail);
+
+        // Thiết lập padding để tránh trùng thanh trạng thái
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
